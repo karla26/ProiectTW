b7e2bd3ed24a15fad5610fce1f5bb98535b1ad15
Karla Andreea <92165261+KarlaChitimia@users.noreply.github.com>
2022-11-28 15:25:38 +0200

    Auth without crypto

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,18 +1,21 @@
 const router = require("express").Router();
 const User = require("../models/User");
 
+
 //REGISTER
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
     const newUser = User({
-       //username : req.body.username,
+        username : req.body.username,
         email :  req.body.email,
         password : req.body.password
     });
 
-
-        const user = newUser.save();
-    res.status(201).json(user); 
-
+    try{
+        const user = await newUser.save();
+        res.status(201).json(user); 
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
 
 module.exports = router; 
