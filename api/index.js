8baebbb901f959d199737a3d81462803bb6c2402
Karla Andreea <92165261+KarlaChitimia@users.noreply.github.com>
2022-11-28 15:26:06 +0200

    auth

--- api/index.js
+++ api/index.js
@@ -2,19 +2,19 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const authRoute = require("../api/routes/auth");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
-mongoose.connect(process.env.MONGO_UR, {
-    useNewUrlParser : true,
-    useUnifiedTopology : true,
-    useCreateIndex : true,
-})
+dotenv.config("./.env");
+
+mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("DB Connection Succesfull!"))
     .catch((err) => console.log(err));
 
+app.use(express.json());
 
 app.use("/api/auth", authRoute);
 
 app.listen(8800, () =>{
     console.log('http://localhost:8800/')
 })
+
--- api/models/User.js
+++ api/models/User.js
@@ -9,4 +9,4 @@ const UserSchema = new mongoose.Schema({
 },
 {timestamps: true});
 
-module.export = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", UserSchema);
